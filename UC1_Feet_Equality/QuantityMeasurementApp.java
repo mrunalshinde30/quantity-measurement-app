ca9e643b524ce4c381390183147abc6c2d287368
mrunalshinde30 <ms4236@srmist.edu.in>
2026-04-22 09:14:31 +0530

    Added UC2: Feet and Inches equality

--- UC1_Feet_Equality/QuantityMeasurementApp.java
+++ UC1_Feet_Equality/QuantityMeasurementApp.java
@@ -1,5 +1,8 @@
+
+
 public class QuantityMeasurementApp {
 
+    // ---------------- UC1: Feet ----------------
     public static class Feet {
         private final double value;
 
@@ -10,17 +13,55 @@ public class QuantityMeasurementApp {
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
 
             Feet other = (Feet) obj;
+
             return Double.compare(this.value, other.value) == 0;
         }
     }
 
-    public static void main(String[] args) {
+    // ---------------- UC2: Inches ----------------
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            Inches other = (Inches) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // ---------------- Demo Methods ----------------
+    public static void demonstrateFeetEquality() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
 
-        System.out.println("Equal? " + f1.equals(f2));
+        System.out.println("Feet Equal: " + f1.equals(f2));
+    }
+
+    public static void demonstrateInchesEquality() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Inches Equal: " + i1.equals(i2));
+    }
+
+    // ---------------- Main Method ----------------
+    public static void main(String[] args) {
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
